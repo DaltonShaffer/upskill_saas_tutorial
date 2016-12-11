4c14197bd962afeca4b3de56af1e5f9395a0c807
Dalton Shaffer <meowmymix@outlook.com>
2016-12-11 20:44:33 +0000

    Added bootstrap to form

--- config/routes.rb
+++ config/routes.rb
@@ -3,3 +3,8 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   resources :contacts
 end
+Rails.application.routes.draw do
+  root to: 'pages#home'
+  get 'about', to: 'pages#about'
+  resources :contacts
+end
